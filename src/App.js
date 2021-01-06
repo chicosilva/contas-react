d964348fd2d0c0adfb20f6a4ebe773e9667d149f
chicosilva <chicosilva1@gmail.com>
2021-01-05 21:26:18 -0300

    contas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import Contas from './components/Contas';
+import Titulo from './components/Titulo';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Titulo titulo="Contas gerais" />
+      <Contas />
+    </>
   );
 }
 
---
+++ src/components/Titulo.js
@@ -0,0 +1,13 @@
+
+function Titulo(props){
+
+    return (
+        <>
+            <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                <h1 class="h2">{props.titulo}</h1>
+            </div>
+        </>
+    )
+}
+
+export default Titulo;
